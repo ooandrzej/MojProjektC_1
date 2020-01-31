0f01ed277e5697ecd5cda8052a27a6286187053e
ooandrzej <ooandrzej@users.noreply.github.com>
2020-01-31 08:50:20 +0100

    commit on 'master'

---
+++ main.c
@@ -0,0 +1,19 @@
+// Moja lekcja programowania
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+	//system("chcp 65001");
+	int tex1, tex2;
+	tex1 = 1;
+	tex2 = 2;
+
+	printf("\n***********************\n");
+	printf("zmienne %s i %.f\n, tex1, tex1");
+	printf("GCC_VERSION = %d.%d.%d \n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+
+	return 0;
+}
