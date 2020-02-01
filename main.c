37b07a9561f5b372d43bd2f4040d6eeb8d41787c
ooandrzej <ooandrzej@users.noreply.github.com>
2020-02-01 19:24:06 +0100

    commit

--- main.c
+++ main.c
@@ -3,17 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int zmiennaA, zmiennaB;
+zmiennaA = 10;
+zmiennaB = 20;
+
 int main()
-
 {
-	system("chcp 65001");
-	char tex1[10] = "ZM1";
-	char tex2[6] = "ZM2";
+	zmiennaA = 11;
+	zmiennaB = 22;
 
-	puts("***********************");
-	printf("zmienne: %s , %s", tex1, tex1);
-	printf("\nWerja GCC: %d.%d.%d \n", __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
-	puts("***********************");
-
+	printf("\nzmienna A = %i, zmienna B = %i\n", zmiennaA, zmiennaB);
+	mojaFunkcja();
 	return 0;
+}
+
+int mojaFunkcja()
+{
+	puts("wynik działania mojej funkcji: mojaFunkcja");
+	printf("\nzmienna A = %i, zmienna B = %i\n", zmiennaA, zmiennaB);
 }
