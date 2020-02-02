ed5e720b8cb673f0c281d541eb4eac14409e0cec
ooandrzej <ooandrzej@users.noreply.github.com>
2020-02-02 10:34:45 +0100

    commit mater

--- main.c
+++ main.c
@@ -4,21 +4,23 @@
 #include <stdlib.h>
 
 int zmiennaA, zmiennaB;
-zmiennaA = 10;
-zmiennaB = 20;
 
 int main()
+
 {
 	zmiennaA = 11;
 	zmiennaB = 22;
 
 	printf("\nzmienna A = %i, zmienna B = %i\n", zmiennaA, zmiennaB);
+
 	mojaFunkcja();
 	return 0;
 }
 
 int mojaFunkcja()
 {
-	puts("wynik działania mojej funkcji: mojaFunkcja");
+	zmiennaA = 11;
+	zmiennaB = 22;
+	printf("wynik działania mojej funkcji: mojaFunkcja");
 	printf("\nzmienna A = %i, zmienna B = %i\n", zmiennaA, zmiennaB);
 }
