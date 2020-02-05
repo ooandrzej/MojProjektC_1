eaf0752a127febd648cb1f857f4cc7b0d4c1d698
ooandrzej <ooandrzej@users.noreply.github.com>
2020-02-05 22:57:45 +0100

    commit

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 //obzar tetowy
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-	printf("\n\n____oto demo____\n\n");
+	double a, b, wynik;
+
+	printf("\npierwzy bok trojkata: ");
+	scanf("%lf", &a);
+	printf("drugi bok: ");
+	scanf("%lf", &b);
+
+	wynik = sqrt(((pow(a, 2)) + (pow(b, 2))));
+
+	printf("najdluzy bok tego trojkata ma dlugoc: %f\n\n", wynik);
 
 	return 0;
 }
