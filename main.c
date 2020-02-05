b72a4312d3ee7bee2ef18f82ce271811a33fcdb8
ooandrzej <ooandrzej@users.noreply.github.com>
2020-02-05 16:32:55 +0100

    commit

--- main.c
+++ main.c
@@ -1,25 +1,9 @@
-/* Moja lekcja programowania*/
-
+//obzar tetowy
 #include <stdio.h>
-#include <stdlib.h>
-
-int zmienna1, zmienna2, zmienna3;
-zmienna3 = 100;
 
 int main()
-
 {
-	zmienna1 = 3;
-	zmienna2 = 7;
+	printf("\n\n____oto demo____\n\n");
 
-	zmienna1 == 3 ? puts("zmienna nr1 dokladnie = 3") : puts("zmienna nie równa ię 3");
-	printf("-suma tych zmiennych wynoi: %i", zmienna1 + zmienna2);
-	printf("\n-mojaFuunkcja zwraca wartość: %i", myFuunc());
-	
 	return 0;
-}
-
-int myFuunc()
-{
-	return zmienna3;
 }
