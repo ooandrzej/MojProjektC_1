2d1a3d321d66694e7c26fd09b47c13f7723a0611
ooandrzej <ooandrzej@users.noreply.github.com>
2020-02-25 14:15:47 +0100

    commit

--- SredniaOcen.c
+++ SredniaOcen.c
@@ -2,14 +2,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-struct oceny
-{
-	char przedmiot;
-	int ocena;
-};
-
 int main()
 {
+	// definiownie zmiennych
 	char przedmiot[15][12] = {
 		"Biologia",
 		"Chemia",
@@ -27,19 +22,62 @@ int main()
 		"Filozofia",
 		"Geografia",
 	};
-	int ocena[15][3];
-	int i, j, a, b, c;
-	char *przedmiot;
-	char *ocena;
+	int ocena[15][12];
+	float mid[12];
+	int i, j, o, p;
 
-	for (i = 0; i < 15; i++) //pętla
+
+	// wypełnienie tablicy: ocena[][] i mid[] wartością domyślną = 0
+	for (i = 0; i < 15; i++)
 	{
-		printf("Podaj ocene z przedmiotu  %s: ", przedm[i]);
-		scanf("%s", &oceny[i]);
-		for (a = 0; a < 5; a++)
+		for (j = 0; j < 12; j++)
+			ocena[i][j] = 0;
+		mid[j] = 0;
 		{
-			printf("%s", oceny[i][a]);
 		}
 	}
+
+	// pobieranie ocen od użytkownika - 0 przechodzi do następnego rzędu
+	// tablicy czyli następnnego przedmiotu
+	for (p = 0; p < 15; p++)
+	{
+		for (o = 0; o < 12; o++)
+		{
+			printf("%s, ocena nr_%d: ", przedmiot[p], o + 1);
+			scanf("%d", &ocena[p][o]);
+			if (ocena[p][o] > 6)
+			{
+				printf("nie ma oceny %d: ", ocena[p][o]);
+				o--;
+			}
+			if (ocena[p][o] < 1)
+				break;
+			mid[p] += ocena[p][o];
+			printf(" mid%d = %.2f\n", p, mid[p]);
+		}
+	}
+
+
+	// kontrolne wyświetlenie zapisanych ocen w tablicy: ocena[][]
+	printf("\n\n*** podsumowanie wszystkich zdobytych ocen ***\n");
+	for (i = 0; i < 15; i++)
+	{
+		if (ocena[i][0] >= 1)
+			printf("\n%s: ", przedmiot[i]);
+
+		for (j = 0; j < 12; j++)
+		{
+			if ((ocena[i][j]) >= 1)
+				printf("%d ", ocena[i][j]);
+			else
+				break;
+		}
+	}
+
+	// zatrzymanie wyświetlanego okna aby sie nie zamknęło, Enter zamknie
+	// okno 
+	printf("\n\n ***  KONIEC -> [wpisz cos + ENTER] ***");
+	scanf("%s", &p);
+
 	return 0;
 }
--- asci_no.c
+++ asci_no.c
@@ -1,20 +1,44 @@
-// program 
+// this program shows number ASCI of char and CHAR of number of asci.
 #include <stdio.h>
 
 int main()
-
 {
-	char c;
+	char c[1];
+	int a;
+
 	printf("wpisz znak: ");
-	scanf("%c", &c);
-	printf("ASCI: %i", (int)c);
+	scanf("%c", &c[1]);
+	printf("ASCI: %i", (int)c[1]);
+	printf("\nwpisz nr znaku w tablicy ASCI : ");
+	scanf("%d", &a);
+	printf("znak nr %d to :  %c", a, (char)a);
+	printf("\n\n    Koniec?   [t] / [n] ...: ");
+	scanf("%c ", &c[1]);
+	printf("\n%c", c[1]);
+	// if (c == 't')
 
-int a;
-printf("\nwpisz nr znaku w tablicy ASCI : ");
-scanf("%d", &a);
-printf("znak nr %d to :  %c", a, (char)a);
 
+
+	printf(" \n\nKONIEC.");
 	return 0;
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
