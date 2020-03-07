b4a8eada416602f7a5a535e2ff3e810b1c46b6d4
ooandrzej <ooandrzej@users.noreply.github.com>
2020-03-07 22:41:17 +0100

    commit

--- 1.c
+++ 1.c
@@ -1,4 +1,4 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 
 typedef struct
@@ -29,22 +29,4 @@ void update_course(course *clasa)
 void display_course(course clasa)
 {
     printf("%d\t%s\t%3.2f\n", clasa.id, clasa.title, clasa.hours);
-}
-*/
-/* system example : DIR */
-#include <stdio.h>  /* printf */
-#include <stdlib.h> /* system, NULL, EXIT_FAILURE */
-
-int main()
-{
-    int i;
-    printf("Checking if processor is available...");
-    if (system(NULL))
-        puts("Ok");
-    else
-        exit(EXIT_FAILURE);
-    printf("Executing command DIR...\n");
-    i = system("dir");
-    printf("The value returned was: %d.\n", i);
-    return 0;
 }
--- asci_no.c
+++ asci_no.c
@@ -1,11 +1,15 @@
 // this program shows number ASCI of char and CHAR of number of asci.
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void delay(int milliseconds);
 
 int main()
 {
 	char c[1];
 	int a;
+	system("chcp 65001");
 	system("cls");
 
 	printf("\nznak: ");
@@ -17,6 +21,19 @@ int main()
 	printf("      '%c'", (char)a);
 
 	printf(" \n\n");
+	printf("Program się zamknie za 5 sek...\n");
+	delay(5000);
 
 	return 0;
+}
+
+void delay(int milliseconds)
+{
+	long pause;
+	clock_t now, then;
+
+	pause = milliseconds * (CLOCKS_PER_SEC / 1000);
+	now = then = clock();
+	while ((now - then) < pause)
+		now = clock();
 }
